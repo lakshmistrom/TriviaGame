51a14ae5a1dbcd14a2bc25eefbfbf4c6e974fba3
Sweet Ace <lakshmialeman@gmail.com>
2018-09-10 23:21:50 -0700

    loaded question in for the user

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,57 @@
-// https://opentdb.com/api.php?amount=10
-var queryUrl = "https://opentdb.com/api.php?amount=10";
+// trivia api: https://opentdb.com
+var queryUrl = "https://opentdb.com/api.php?amount=1";
+
+//represents access to api objects
+var access;
+
+//data from the api
+//category
+var category;
+//question
+var question;
+//correct answer
+var correctAnswer;
+//wrong answers
+var wrongAnswers;
+//list of answers
+var answers = [];
+//random chosen answer
+var randomAnswer;
+//requests data from the api
 $.ajax({
     url: queryUrl,
     method: "GET",
-}).then(function(response){
+}).then(function (response) {
     console.log(response);
-    //obtain reference to card text and load question
-    console.log();
-    //access questions from the api
-    var access = response.results[0];
-    //load question
-    var question = access.question;
-    //load correct answer
-    var correctAnswer = access.correct_answer;
-    //load wrong answers
-    var wrongAnswers = access.incorrect_answers;
-    //load category
-    var category = access.category;
-    
+    //load data from the api to global variables
+
+    //accesss all results
+    access = response.results[0];
+    //initialize category
+    category = access.category;
+    //initialize question
+    question = access.question;
+    //initialize correct answer
+    correctAnswer = access.correct_answer;
+    //initialize wrong answers
+    wrongAnswers = access.incorrect_answers;
+    //initialize answers array
+    //add correct answer
+    answers.push(correctAnswer);
+    //add wrong answer
+    answers = answers.concat(wrongAnswers);
+    //shuffles the answers
+    answers.sort(function () { return 0.5 - Math.random() });
+
+    //show question category in html
+    $("#category").html(`${category}`);
+
     //show question in html
-    var qCategory = $("#category").html(`${category}`);
-    var cardTextQuestion = $("#question").html(`${question}`);
-    var answer  = $("#question-options").append("<li class=\"list-group-item bg-dark text-white\">" + `${correctAnswer}` + " <i class=\"fas fa-angle-left text-secondary\"></i></li>");
-    for(var i = 0; i < wrongAnswers.length; i++){
-        $("#question-options").append("<li class=\"list-group-item bg-dark text-white\">" + `${wrongAnswers[i]}` + " <i class=\"fas fa-angle-left text-secondary\"></i></li>");
+    $("#question").html(`${question}`);
+
+    //iterate over  answers
+    for (var i = 0; i < answers.length; i++) {
+        //show  answers in html
+        $("#question-options").append("<li class=\"list-group-item bg-dark text-white\">" + `${answers[i]}` + " <i class=\"fas fa-angle-left text-secondary\"></i></li>");
     }
-    
-    // var question = cardText.text(response.question);
-    console.log(question);
-    console.log(question.correct_answer);
 });
