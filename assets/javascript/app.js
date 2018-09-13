1903c7a3808734d3e0bd2043e78b32ee88718476
Sweet Ace <lakshmialeman@gmail.com>
2018-09-13 02:01:33 -0700

    loaded the timer in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,92 +26,188 @@ var randomAnswer;
 //current answer
 var currentAnswer
 
+//  Variable that will hold the setInterval that runs the countdown
+var intervalId;
+
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+
 //requests data from the api
-$.ajax({
-    url: queryUrl,
-    method: "GET",
-}).then(function (response) {
-    console.log(response);
-    //load data from the api to global variables
+function getQuestion() {
+    return $.ajax({
+        url: queryUrl,
+        method: "GET",
+    }).then(function (response) {
+        //load data from the api to global variables
+        //accesss all results
+        access = response.results[0];
 
-    //accesss all results
-    access = response.results[0];
-    //initialize category
-    category = access.category;
-    //initialize question
-    question = access.question;
-    //initialize correct answer
-    correctAnswer = access.correct_answer;
-    //initialize wrong answers
-    wrongAnswers = access.incorrect_answers;
-    //initialize answers array
-    //add correct answer
-    answers.push(correctAnswer);
-    //add wrong answer
-    answers = answers.concat(wrongAnswers);
-    //shuffles the answers
-    answers.sort(function () { return 0.5 - Math.random() });
+        //initialize category
+        category = access.category;
 
-    //show question category in html
-    $("#category").html(`${category}`);
+        //initialize question
+        question = access.question;
 
-    //show question in html
-    $("#question").html(`${question}`);
+        //initialize correct answer
+        correctAnswer = access.correct_answer;
 
-    //iterate over  answers
-    for (var i = 0; i < answers.length; i++) {
-        //show answers in html
-        $("#question-options").append("<li class=\"list-group-item bg-dark text-white answer\" onmouseover=\"mouseOver(this)\" onmouseout=\"mouseOut(this)\">" + `${answers[i]}` + " <i class=\"fas fa-angle-left text-secondary\"></i></li>");
+        //initialize wrong answers
+        wrongAnswers = access.incorrect_answers;
 
-    }
-    //remove click handler
-    $(".answer").off("click");
+        //initialize answers array
+        answers = [];
 
-    //handles choosing the answer
-    $(".answer").click(function () {
-        //console.log(this);
-        //initialize value of the question from the current element
-        currentAnswer = $(this).text().trim();
+        //add correct answer
+        answers.push(correctAnswer);
 
-        //console.log(currentAnswer);
-        //the user chooses a question
-        //if the user chooses the right question let them know they chose the right answer,move onto the next question and restart the timer
-        if (currentAnswer === correctAnswer) {
-            //sets the right answer check mark
-            $(this).find(".fa-angle-left").addClass("fa-check text-success").removeClass("fa-angle-left text-white");
+        //add wrong answer
+        answers = answers.concat(wrongAnswers);
 
-            //set the wrong answer x's
-            $("#question-options").find(".fa-angle-left").addClass("fa-times text-danger").removeClass("fa-angle-left text-white");
-        } else {
-            //the user chooses the wrong question let them know they chose the wrong answer, move onto the next question and restart the timer
-            $("#question-options").find(".list-group-item").each(function(index, element){
-                currentAnswer = $(element).text().trim();
-                //set the right answer
-                if (currentAnswer === correctAnswer) {
-                    $(this).find(".fa-angle-left").addClass("fa-check text-success").removeClass("fa-angle-left text-white");
-                } else {
-                    //set the wrong answer x's
-                    $("#question-options").find(".fa-angle-left").addClass("fa-times text-danger").removeClass("fa-angle-left text-white");
-                }
-                console.log(element);
-            });
+        //shuffles the answers
+        answers.sort(function () { return 0.5 - Math.random() });
 
+        //show question category in html
+        $("#category").html(`${category}`);
+
+        //show question in html
+        $("#question").html(`${question}`);
+
+        //empty questions
+        $("#question-options").empty();
+
+        //iterate over answers
+        for (var i = 0; i < answers.length; i++) {
+            //show answers in html
+            $("#question-options").append("<li class=\"list-group-item bg-dark text-white answer\" onmouseover=\"mouseOver(this)\" onmouseout=\"mouseOut(this)\">" + `${answers[i]}` + " <i class=\"fas fa-angle-left text-secondary\"></i></li>");
         }
 
-        //if the there was no answer chosen move onto the next question and restart the timer
+        //remove click handler
+        $(".answer").off("click");
+
+        //handles choosing the answer
+        $(".answer").click(function () {
+            //initialize value of the question from the current element
+            currentAnswer = $(this).text().trim();
+
+            //let the user know they chose the right answer
+            if (currentAnswer === correctAnswer) {
+                //sets the right answer check mark
+                $(this).find(".fa-angle-left").addClass("fa-check text-success").removeClass("fa-angle-left text-white");
+
+                //set the wrong answer x's
+                $("#question-options").find(".fa-angle-left").addClass("fa-times text-danger").removeClass("fa-angle-left text-white");
+
+                //stops countdown
+                countdown.stop();
+
+                //delaying showing the answer to the user and initialize the game again
+                setTimeout(initGame, 2000);
+            } else {
+                //let the user know they chose the wrong answer
+                //show the right and wrong answers
+                rightAndWrong();
+
+                //stops countdown
+                countdown.stop();
+
+                //delaying showing the answer to the user and initialize the game again
+                setTimeout(initGame, 2000);
+            }
+        });
     });
-});
+};
+// coundown object
+var countdown = {
+    //countdown time initialized
+    time: 59,
+
+    //resets countdown
+    reset: function () {
+        //resets countdown time
+        countdown.time = 59;
+
+        //change the html to read the current countdown time
+        $("#countDown").text("59");
+
+    },
+    //starts the countdown
+    start: function () {
+        // use setInterval to start the count here and set the clock to running.
+        if (!clockRunning) {
+            //start countdown
+            intervalId = setInterval(countdown.count, 1000);
+
+            //the countdown has started
+            clockRunning = true;
+        }
+    },
+    //stops countdown
+    stop: function () {
+        // use clearInterval to stop the count here and set the clock to not be running.
+        //reset interval
+        clearInterval(intervalId);
+
+        //stop the countdown
+        clockRunning = false;
+    },
+    //keep track of the countdown
+    count: function () {
+        // decrease time by 1
+        countdown.time--;
+
+        // update html with the current countdown
+        $("#countDown").text(countdown.time);
+
+        //if count <= 0 then the countdown has reached the end, show the right/wrong answers and restart the timer
+        if (countdown.time <= 0) {
+            //stop countdown
+            countdown.stop();
+
+            //display the right and wrong answers
+            rightAndWrong();
+
+            //delaying showing the answer to the user and initialize the game again
+            setTimeout(initGame, 2000);
+        }
+    }
+};
 
 //remove click handler
 $("#startBtn").off("click");
 
-//initiate the game
-$("#startBtn").click(function () {
-    //show restart button
-    $("#game").removeClass("invisible");
-    $("#startBtn").addClass("invisible")
-});
+//initiate the game after clicking the start button
+$("#startBtn").click(initGame);
 
+//initialize the game
+function initGame() {
+    //loads question
+    getQuestion().done(function () {
+        //show restart button
+        $("#game").removeClass("invisible");
+        $("#startBtn").addClass("invisible");
+
+        //handles countdown
+        countdown.reset();
+        countdown.start();
+    });
+}
+//displays the wrong and right answers
+function rightAndWrong() {
+    //reads each answer
+    $("#question-options").find(".list-group-item").each(function (index, element) {
+        //initializes the current answer
+        currentAnswer = $(element).text().trim();
+
+        //set the right answer with green check marks
+        if (currentAnswer === correctAnswer) {
+            $(this).find(".fa-angle-left").addClass("fa-check text-success").removeClass("fa-angle-left text-white");
+        } else {
+            //set the wrong answer with red x's
+            $(this).find(".fa-angle-left").addClass("fa-times text-danger").removeClass("fa-angle-left text-white");
+        }
+        //console.log(element);
+    });
+}
 //mouse over list of questions
 function mouseOver(element) {
     //makes changes to the background color of the question when user places mouse over it
